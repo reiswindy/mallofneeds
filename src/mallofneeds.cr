771be2dcfdfcb1aba84fc112488519bb33671988
reiswindy <megamanbn@gmail.com>
2018-08-21 20:33:38 -0500

    Fix movement

--- src/mallofneeds.cr
+++ src/mallofneeds.cr
@@ -30,6 +30,7 @@ module Mallofneeds
       window.vertical_sync_enabled = true
       @window = window
       @player = Player.new({SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2})
+      @items = [] of Item
     end
 
     def process_events
@@ -58,9 +59,9 @@ module Mallofneeds
 
     def run
       while @window.open?
-        update
         process_events
-        render          
+        update
+        render
       end
     end
   end
@@ -71,9 +72,11 @@ module Mallofneeds
     SPEED = 4
 
     @sprite : SF::Sprite
-    @speed_x : Int32
-    @speed_y : Int32
     @speed_multiplier = 1
+    @moving_up = false
+    @moving_down = false
+    @moving_left = false
+    @moving_right = false
 
     def initialize(position)
       sprite = SF::Sprite.new
@@ -81,14 +84,17 @@ module Mallofneeds
       sprite.texture_rect = SF.int_rect(0, 0, 15, 13)
       sprite.position = position
       @sprite = sprite
-      @speed_x = 0
-      @speed_y = 0
     end
 
     def update
+      speed_x = speed_y = 0
+      speed_y += SPEED * @speed_multiplier * (-1) if @moving_up
+      speed_x += SPEED * @speed_multiplier * (-1) if @moving_left
+      speed_y += SPEED * @speed_multiplier if @moving_down
+      speed_x += SPEED * @speed_multiplier if @moving_right
       old_x, old_y = @sprite.position
-      new_x = old_x + @speed_x
-      new_y = old_y + @speed_y
+      new_x = old_x + speed_x
+      new_y = old_y + speed_y
       new_x = 0 if new_x < 0
       new_x = Game::SCREEN_WIDTH - @sprite.global_bounds.width if new_x + @sprite.global_bounds.width > Game::SCREEN_WIDTH
       new_y = 0 if new_y < 0
@@ -97,17 +103,17 @@ module Mallofneeds
     end
 
     def key_pressed(key)
-      @speed_y = SPEED * @speed_multiplier * (-1) if key.w?
-      @speed_x = SPEED * @speed_multiplier * (-1) if key.a?
-      @speed_y = SPEED * @speed_multiplier if key.s?
-      @speed_x = SPEED * @speed_multiplier if key.d?
+      @moving_up = true if key.w?
+      @moving_down = true if key.s?
+      @moving_left = true if key.a?
+      @moving_right = true if key.d?
     end
 
     def key_released(key)
-      @speed_y = 0 if key.w?
-      @speed_x = 0 if key.a?
-      @speed_y = 0 if key.s?
-      @speed_x = 0 if key.d?
+      @moving_up = false if key.w?
+      @moving_down = false if key.s?
+      @moving_left = false if key.a?
+      @moving_right = false if key.d?
     end
 
     def draw(target, states)
@@ -118,13 +124,15 @@ module Mallofneeds
   abstract class Item
     include SF::Drawable
 
-    @lifetime : Int32
+    @fun : Int32
     @price : Int32
+    @health : Int32
+    @lifetime : Int32
     @label : SF::Text
     @clock : SF::Clock
     @sprite : SF::Sprite
 
-    def initialize(@lifetime, @price, position)
+    def initialize(@lifetime, @price, @fun, @health, position)
       sprite = SF::Sprite.new
       sprite.texture = Game::SPRITESHEET
       sprite.position = position
@@ -146,8 +154,10 @@ module Mallofneeds
     end
 
     class Medicine < Item
+      FUN = 0
+      HEALTH = 30
       def initialize(lifetime, price, position)
-        super(lifetime, price, position)
+        super(lifetime, price, FUN, HEALTH, position)
         @sprite.texture_rect = SF.int_rect(16, 0, 14, 22)
         x, y = position
         @label.position = {x + (@sprite.global_bounds.width / 2).to_i - (@label.global_bounds.width / 2).to_i, y + 22 + 2}
@@ -155,8 +165,10 @@ module Mallofneeds
     end
 
     class Food < Item
+      FUN = 10
+      HEALTH = 20
       def initialize(lifetime, price, position)
-        super(lifetime, price, position)
+        super(lifetime, price, FUN, HEALTH, position)
         @sprite.texture_rect = SF.int_rect(32, 0, 20, 20)
         x, y = position
         @label.position = {x + (@sprite.global_bounds.width / 2).to_i - (@label.global_bounds.width / 2).to_i, y + 20 + 2}
@@ -164,8 +176,10 @@ module Mallofneeds
     end
 
     class Disc < Item
+      FUN = 30
+      HEALTH = 0
       def initialize(lifetime, price, position)
-        super(lifetime, price, position)
+        super(lifetime, price, FUN, HEALTH, position)
         @sprite.texture_rect = SF.int_rect(53, 0, 20, 20)
         x, y = position
         @label.position = {x + (@sprite.global_bounds.width / 2).to_i - (@label.global_bounds.width / 2).to_i, y + 20 + 2}
